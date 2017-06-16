6d314472a726a59d4c2d81d3148419672f4b6ab7
Coen <boelhouwers.coen@gmail.com>
2017-06-16 15:14:29 +0200

    Implemented simple snake.

--- src/games/AnacondaGame.java
+++ src/games/AnacondaGame.java
@@ -1,12 +1,13 @@
 package games;
 
-import games.GamePane;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.TextAlignment;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Coen Boelhouwers
@@ -14,26 +15,87 @@ import javafx.scene.text.TextAlignment;
 public class AnacondaGame extends GamePane {
 
 	private long lastNanos;
-	private double i;
+	private List<Snake> snakes;
 
 	public AnacondaGame() {
 		super("Anaconda");
-		i = 400;
+
+		snakes = new ArrayList<>();
+		for (int i = 0; i < 10; i++) {
+			Snake snake = new Snake();
+			snake.x = Math.random() * 900;
+			snakes.add(snake);
+		}
+
 		setBackground(new Background(new BackgroundFill(Color.TRANSPARENT, null, null)));
 	}
 
 	@Override
 	public void update(double elapsedTime) {
-		i -= 50 * elapsedTime;
-		if (i < -50) i = getHeight();
+		snakes.forEach(snake -> {
+			snake.update(elapsedTime);
+			if (snake.x < -snake.length) {
+				snake.x = getWidth();
+				snake.y = snake.width + Math.random() * (getHeight() - snake.width);
+			}
+		});
 	}
 
 	@Override
 	public void draw(GraphicsContext g) {
 		g.clearRect(0, 0, getWidth(), getHeight());
-		g.setFill(Color.GREEN);
-		g.setFont(Font.font("Roboto Medium", 200));
-		g.setTextAlign(TextAlignment.CENTER);
-		g.fillText("TEST!", getWidth()/2, i);
+		snakes.forEach(snake -> snake.draw(g));
+	}
+
+	private static class Snake {
+		double x;
+		double y;
+		double length;
+		double width;
+		private double speed;
+		private static Image head = new Image("/snake_head.png", true);
+		private static final Image body_0 = new Image("/snake_middle_straight.png", true);
+		private static final Image body_1 = new Image("/snake_middle_left.png", true);
+		private static final Image body_2 = new Image("/snake_middle_right.png", true);
+		private static Image tail = new Image("/snake_tail.png", true);
+
+		private List<Image> segments;
+
+		public Snake() {
+			speed = 50 + Math.random() * 200;
+			segments = new ArrayList<>();
+			segments.add(head);
+			for (int i = (int) (Math.random() * 6); i > 0; i--) {
+				switch (i % 3) {
+					case 0:
+						segments.add(body_0);
+						break;
+					case 1:
+						segments.add(body_1);
+						break;
+					case 2:
+						segments.add(body_2);
+						break;
+				}
+			}
+			segments.add(tail);
+		}
+
+		public void draw(GraphicsContext g) {
+			g.save();
+			g.translate(x, y);
+			g.rotate(-90);
+			g.scale(0.5, 0.5);
+			for (int i = 0; i < segments.size(); i++) {
+				g.drawImage(segments.get(i), 0, head.getHeight() * i);
+			}
+			g.restore();
+			width = head.getWidth();
+			length = head.getWidth() * segments.size();
+		}
+
+		public void update(double elapsedTime) {
+			x -= speed * elapsedTime;
+		}
 	}
 }
